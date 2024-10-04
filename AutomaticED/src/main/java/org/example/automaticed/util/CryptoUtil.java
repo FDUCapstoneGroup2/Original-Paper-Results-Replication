329ac6257034140cd4c0ec986bb9e7e862a241d2
Liam6v <148597185+Liam6v@users.noreply.github.com>
2024-10-04 15:53:59 -0700

    Update CryptoUtil.java
    I changes two lines of code which have commented, it has tested working when you encrypt or decrypt without using database.

--- AutomaticED/src/main/java/org/example/automaticed/util/CryptoUtil.java
+++ AutomaticED/src/main/java/org/example/automaticed/util/CryptoUtil.java
@@ -7,11 +7,12 @@ import java.util.Base64;
 public class CryptoUtil {
 
 
-    private static final String TRANSFORMATION = "DES";
+    // private static final String TRANSFORMATION = "DES"; 
+    private static final String TRANSFORMATION = "DES/ECB/PKCS5Padding"; //The TRANSFORMATION should be DES/ECB/PKCS5Padding, which is just like in the DESSensitiveConverter
 
-
-    private static final String SECRET_KEY = "mySuperSecretKey";
-
+    // private static final String SECRET_KEY = "mySuperSecretKey";
+    private static final String SECRET_KEY = "mySuperS"; //The SECRET_KEY should be 8 bytes long
+    
     public static String encrypt(String input,String algorithm) throws Exception {
         SecretKeySpec keySpec = new SecretKeySpec(SECRET_KEY.getBytes(), algorithm);
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
